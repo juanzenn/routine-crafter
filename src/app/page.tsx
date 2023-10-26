6c6a39f0b40fbb50bbdd8d544bf425175f8476cd
Juan Alvarez <juanandres140299@gmail.com>
2023-10-26 19:35:05 -0400

    (chore): add prisma

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
-export default function Home() {
-  return <main className="min-h-screen p-24">Homepage</main>;
+import { db } from "@/lib/db";
+
+export default async function Home() {
+  const users = await db.user.findMany();
+
+  return (
+    <main className="min-h-screen p-24">
+      <pre>{JSON.stringify(users, null, 2)}</pre>
+    </main>
+  );
 }
