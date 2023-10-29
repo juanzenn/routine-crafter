30aebdee01d87cb028d0b1b5d4da43bb3aaaaa77
Juan Alvarez <juanandres140299@gmail.com>
2023-10-29 11:44:06 -0400

    (hotfix): env

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -5,23 +5,21 @@ import { type Provider } from "next-auth/providers";
 import EmailProvider from "next-auth/providers/email";
 import { authConfig } from "./auth.config";
 
-const { EMAIL_FROM = "" } = process.env;
+const {
+  EMAIL_HOST = "",
+  EMAIL_PORT = "",
+  EMAIL_USER = "",
+  EMAIL_PASSWORD = "",
+  EMAIL_FROM = "",
+} = process.env;
+
+const EMAIL_SERVER = `smtp://${EMAIL_USER}:${EMAIL_PASSWORD}@${EMAIL_HOST}:${EMAIL_PORT}`;
 
 export const nextAuth = NextAuth({
   ...authConfig,
   session: { strategy: "jwt" },
   providers: [
-    EmailProvider({
-      server: {
-        host: "smtp.gmail.com",
-        port: 587,
-        auth: {
-          user: "juanandres140299@gmail.com",
-          pass: "kheurrghbqvpacqj",
-        },
-      },
-      from: EMAIL_FROM,
-    }),
+    EmailProvider({ server: EMAIL_SERVER, from: EMAIL_FROM }),
   ] as Provider[],
   adapter: PrismaAdapter(db),
 });
