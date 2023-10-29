416ee382063dc6a16167bd2f5a70d22c17d701ab
Juan Alvarez <juanandres140299@gmail.com>
2023-10-29 17:56:24 -0400

    (chore): update homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,40 +1,40 @@
+import { cn } from "@/lib/utils";
 import { buttonVariants } from "@/ui/Button";
 import Image from "next/image";
 import Link from "next/link";
 
 export default async function Home() {
   return (
-    <main className="py-16 min-h-full flex flex-col justify-center">
-      <div className="container mx-auto px-4 text-center">
-        <figure className="relative mx-auto mb-8 aspect-video max-w-[50%]">
+    <main className="h-full">
+      <section className="flex flex-row-reverse h-full">
+        <figure className="relative mx-auto flex-1 h-full border-l border-slate-300 shadow-sm">
           <Image
-            fill={true}
-            src="https://images.unsplash.com/photo-1520948013839-62020f374478"
+            fill
+            src="https://images.unsplash.com/photo-1606328500899-38351f33df8a?w=1200"
             alt="Person lifting weights"
-            className="shadow-lg rounded-md"
           />
         </figure>
 
-        <h1 className="text-primary text-4xl font-bold mb-4">
-          Routine Crafter - Your Ultimate Weightlifting Companion
-        </h1>
-        <p className="text-primary/70 text-lg mb-8">
-          Effortlessly Create and Manage Exercises and Routines for Optimal
-          Weightlifting Progress
-        </p>
+        <section className="flex-1 flex flex-col justify-center px-8 lg:px-12 xl:px-24">
+          <h1 className="text-slate-900 text-6xl tracking-tight font-bold mb-4">
+            Routine Crafter
+          </h1>
+          <p className="text-slate-600 text-xl mb-8 max-w-lg">
+            Your Ultimate Weightlifting Companion. Effortlessly Create and
+            Manage Exercises and Routines for Optimal Weightlifting Progress
+          </p>
 
-        <div className="flex gap-6 mx-auto w-fit">
           <Link
-            className={buttonVariants({ size: "lg", variant: "ghost" })}
+            className={cn(
+              buttonVariants({ size: "lg" }),
+              "text-2xl p-6 px-10 w-3/4 mx-auto"
+            )}
             href="/login"
           >
-            Sign Up
+            Get Started
           </Link>
-          <Link className={buttonVariants({ size: "lg" })} href="/login">
-            Login
-          </Link>
-        </div>
-      </div>
+        </section>
+      </section>
     </main>
   );
 }
